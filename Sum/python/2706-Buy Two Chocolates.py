ebf40679a89ef8f3aed7bc4ef77fdebb6c589ad5
Prithvi <prithhvisr003@gmail.con>
2023-12-21 07:13:22 +0530

    2706 - Buy Two chocolates
    Leetcode Question 2706 BUy two Chocolates

---
+++ Sum/python/2706-Buy Two Chocolates.py
@@ -0,0 +1,18 @@
+'''
+You are given an integer array prices representing the prices of various chocolates in a store.
+You are also given a single integer money, which represents your initial amount of money.
+You must buy exactly two chocolates in such a way 
+that you still have some non-negative leftover money.
+You would like to minimize the sum of the prices of the two chocolates you buy.
+Return the amount of money you will have leftover after buying the two chocolates. 
+If there is no way for you to buy two chocolates without ending up in debt, return money. 
+Note that the leftover must be non-negative.
+'''
+
+class Solution:
+    def buyChoco(self, prices: list[int], money: int) -> int:
+        price1 = min(prices)
+        prices.remove(price1)
+        price2 = min(prices)
+        leftover = money - (price1 + price2)
+        return leftover if leftover >= 0 else money
