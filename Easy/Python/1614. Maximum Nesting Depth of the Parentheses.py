6e9f9227fcab03dc8ea5279f2a9a3d33831225f9
Prithvi824 <Prithvisr003@gmail.com>
2024-04-04 15:02:14 +0530

    1614. Maximum Nesting Depth of the Parentheses

---
+++ Easy/Python/1614. Maximum Nesting Depth of the Parentheses.py
@@ -0,0 +1,31 @@
+"""
+A string is a valid parentheses string (denoted VPS) if it meets one of the following:
+
+It is an empty string "", or a single character not equal to "(" or ")",
+It can be written as AB (A concatenated with B), where A and B are VPS's, or
+It can be written as (A), where A is a VPS.
+We can similarly define the nesting depth depth(S) of any VPS S as follows:
+
+depth("") = 0
+depth(C) = 0, where C is a string with a single character not equal to "(" or ")".
+depth(A + B) = max(depth(A), depth(B)), where A and B are VPS's.
+depth("(" + A + ")") = 1 + depth(A), where A is a VPS.
+For example, "", "()()", and "()(()())" are VPS's (with nesting depths 0, 1, and 2), and ")(" and "(()" are not VPS's.
+
+Given a VPS represented as string s, return the nesting depth of s.
+"""
+
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        left = 0
+        right = 0
+        ans = 0
+
+        for letter in s:
+            if letter == "(":
+                left += 1
+            elif letter == ")":
+                right += 1
+            ans = ans if ans > (left-right) else (left-right)
+
+        return ans
